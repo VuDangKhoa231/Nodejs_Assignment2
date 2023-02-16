d3ea5f28b6846aca3c19abb7ff92ad40351fc9ec
Thien0303 <lethien0330@gmail.com>
2023-02-17 00:10:31 +0700

    update nations

--- model/nation.js
+++ model/nation.js
@@ -1,26 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-var commentSchema = new Schema(
-  {
-    rating: {
-      type: Number,
-      min: 1,
-      max: 5,
-      required: true,
-    },
-    comment: {
-      type: String,
-      required: true,
-    },
-    author: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
 
 var nationSchema = new Schema(
   {
@@ -31,7 +10,7 @@ var nationSchema = new Schema(
     },
     description: {
       type: String,
-      // required: true,
+     required: true,
     },
     // comments: [commentSchema],
   },
